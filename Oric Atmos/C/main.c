4038032ce8d36da3d2bafcd86f6294ce5c6e924a
Xander Mol <xander@xandermol.com>
2021-01-27 23:48:32 +0100

    20210127-2348

--- Oric Atmos/C/main.c
+++ Oric Atmos/C/main.c
@@ -1,12 +1,19 @@
 #include <lib.h>
 
+/* External functions */
+extern char *strchr(char *s,char c); /* from string.h */
+extern char *strcat(char *s1,char *s2); /* from string.h */
+
 /* Functions */
 void loadmainscreen();
 void windowsave(unsigned char ypos, unsigned char height);
 void windowrestore();
 void menumakeborder(unsigned char xpos, unsigned char ypos, unsigned char height, unsigned char width);
 void menuplacebar();
+char menupulldown(char xpos, char ypos, char menunumber);
+char menumain();
 char* screenpos(unsigned char xpos, unsigned char ypos);
+char getkey(char* allowedkeys, char joystickmask);
 
 /* Variables */
 struct WindowStruct
@@ -21,29 +28,38 @@ int windowaddress = 0xa100;
 unsigned char windownumber = 1;
 
 char menubaroptions = 4;
+char pulldownmenunumber = 7;
 char menubartitles[4][12] = {"Game","Disc","Music","Information"};
-char submenuoptions[4] = {3,2,3,1};
-char submenutitles[4][3][11] = {
-    {"Throw dice","Restart","Stop"},
-    {"Save game","Load game"},
-    {"Next","Stop","Restart"},
-    {"Credits"}
+char menubarcoords[4] = {1,6,11,17};
+char pulldownmenuoptions[7] = {3,2,3,1,2,2,3};
+char pulldownmenutitles[7][3][15] = {
+    {"Throw dice",
+     "Restart   ",
+     "Stop      "},
+    {"Save game",
+     "Load game"},
+    {"Next   ",
+     "Stop   ",
+     "Restart"},
+    {"Credits"},
+    {"Yes",
+     "No "},
+    {"Restart"
+    ,"Stop   "},
+    {"Continue game",
+     "New game     ",
+     "Stop         "}
 };
 
-char pulldownmenunumber = 3;
-char pulldownmenuoptions[3] = {2,2,3};
-char pulldownmenutitles[3][3][15] = {
-    {"Yes","No"},
-    {"Restart","Stop"},
-    {"Continue game","New game","Stop"}
-};
+char numbers[11]="0123456789";
+char letters[53]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char updownenter[4] = {10,11,13,0};
+char leftright[3] = {8,9,0};
 
 /* Main routine */
 
 void main()
 {
-    unsigned char x;
-
     setflags(SCREEN);
     paper(0);
     ink(1);
@@ -51,19 +67,11 @@ void main()
 
     loadmainscreen();
     menuplacebar();
-
-    for(x=0; x<5 ; x++)
-    {
-        menumakeborder(18,2+x*2,6,20);
-        sprintf(screenpos(20,4+x*2),"%cWindow %d%c", A_FWYELLOW, x+1, A_FWRED);
-        sprintf(screenpos(20,5+x*2),"%cPress a key.%c", A_FWYELLOW, A_FWRED);
-        get();
-    }
-    for(x=0; x<5 ; x++)
-    {
-        windowrestore();
-        get();
-    }
+    menumakeborder(18,8,6,20);
+    sprintf(screenpos(20,10),"%cLoad old game?%c",A_FWYELLOW, A_FWRED);
+    menupulldown(27,11,5);
+    windowrestore();
+    menumain();
 }
 
 /* Game routines */
@@ -144,6 +152,161 @@ void menuplacebar()
     }
 }
 
+char menupulldown(char xpos, char ypos, char menunumber)
+{
+    char* screen;
+    char x;
+    char validkeys[6];
+    char key;
+    char joymask = 3;
+    char exit = 0;
+    char menuchoice = 1;
+
+    windowsave(ypos, pulldownmenuoptions[menunumber]+4);
+    if(menunumber>menubaroptions)
+    {
+        screen = screenpos(xpos,ypos);
+        sprintf(screen,"%c%c",A_FWRED,125);
+        screen+=2;
+        for(x=0;x<strlen(pulldownmenutitles[menunumber-1][0])+4;x++) { sprintf(screen++,"%c",94); }
+    }
+    screen = screenpos(xpos,ypos+1);
+    for(x=0;x<pulldownmenuoptions[menunumber-1];x++)
+    {
+        sprintf(screen,"%c%c%c%c%s%c%c%c",
+            A_FWRED,91, A_BGCYAN,A_FWBLACK,
+            pulldownmenutitles[menunumber-1][x],
+            A_FWRED,91,A_BGBLACK);
+        screen+=40;
+    }
+    sprintf(screen,"%c%c",A_FWRED,123);
+    screen+=2;
+    for(x=0;x<strlen(pulldownmenutitles[menunumber-1][0])+4;x++) { sprintf(screen++,"%c",93); }
+
+    strcpy(validkeys, updownenter);
+    if(menunumber<=menubaroptions)
+    {
+        strcat(validkeys, leftright);
+        joymask+=12;
+    }
+    
+    do
+    {
+        screen = screenpos(xpos+2,ypos+menuchoice);
+        sprintf(screen,"%c%c%s%c%c%c",
+            A_BGYELLOW,A_FWBLACK,
+            pulldownmenutitles[menunumber-1][menuchoice-1],
+            A_FWRED,91,A_BGBLACK);
+        key = getkey(validkeys, joymask);
+        switch (key)
+        {
+        case 13:
+            exit = 1;
+            break;
+        
+        case 8:
+        case 9:
+            exit = 1;
+            menuchoice = 10 + key;
+            break;
+
+        case 10:
+        case 11:
+            screen = screenpos(xpos+2,ypos+menuchoice);
+            sprintf(screen,"%c%c%s%c%c%c",
+                A_BGCYAN,A_FWBLACK,
+                pulldownmenutitles[menunumber-1][menuchoice-1],
+                A_FWRED,91,A_BGBLACK);
+            if(key==11)
+            {
+                menuchoice--;
+                if(menuchoice<1)
+                {
+                    menuchoice=pulldownmenuoptions[menunumber-1];
+                }
+            }
+            else
+            {
+                menuchoice++;
+                if(menuchoice>pulldownmenuoptions[menunumber-1])
+                {
+                    menuchoice = 1;
+                }
+            }
+            break;
+
+        default:
+            break;
+        }
+    } while (exit==0);
+    windowrestore();    
+    return menuchoice;
+}
+
+char menumain()
+{
+    char menubarchoice = 1;
+    char menuoptionchoice = 0;
+    char key;
+    char validkeys[4] = {8,9,13,0};
+    char xpos;
+
+    do
+    {
+        do
+        {
+            sprintf(screenpos(menubarcoords[menubarchoice-1],0),
+                "%c%s%c", A_BGWHITE,
+                menubartitles[menubarchoice-1], A_BGGREEN);
+            key = getkey(validkeys, 12);
+            sprintf(screenpos(menubarcoords[menubarchoice-1],0),
+                "%c%s", A_BGGREEN,
+                menubartitles[menubarchoice-1]);
+            if(key==8)
+            {
+                menubarchoice--;
+                if(menubarchoice<1)
+                {
+                    menubarchoice = menubaroptions;
+                }
+            }
+            else if (key==9)
+            {
+                menubarchoice++;
+                if(menubarchoice>menubaroptions)
+                {
+                    menubarchoice = 1;
+                }
+            }
+        } while (key!=13);
+        xpos=menubarcoords[menubarchoice-1]-1;
+        if(xpos+strlen(pulldownmenutitles[menubarchoice-1][0])>38)
+        {
+            xpos=menubarcoords[menubarchoice-1]+strlen(menubartitles[menubarchoice-1])-strlen(pulldownmenutitles[menubarchoice-1][0]);
+        }
+        menuoptionchoice = menupulldown(xpos,0,menubarchoice);
+        if(menuoptionchoice==18)
+        {
+            menuoptionchoice=0;
+            menubarchoice--;
+            if(menubarchoice<1)
+            {
+                    menubarchoice = menubaroptions;
+            }
+        }
+        if(menuoptionchoice==19)
+        {
+            menuoptionchoice=0;
+            menubarchoice++;
+            if(menubarchoice>menubaroptions)
+            {
+                menubarchoice = 1;
+            }
+        }
+    } while (menuoptionchoice==0);
+    return menubarchoice*10+menuoptionchoice;    
+}
+
 /* Generic screen functions */
 
 char* screenpos(unsigned char xpos, unsigned char ypos)
@@ -156,4 +319,17 @@ char* screenpos(unsigned char xpos, unsigned char ypos)
        Output: pointer to correct screen memoery position */
        
     return (char*)0xbba8 + ypos*40 + xpos;
+}
+
+/* Generic input routines */
+
+char getkey(char* allowedkeys, char joystickmask)
+{
+    char key;
+
+    do
+    {
+        key = get();
+    } while (strchr(allowedkeys, key)==0);
+    return key;
 }
